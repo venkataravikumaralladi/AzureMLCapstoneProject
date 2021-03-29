435acbb82ed49ce3b5c468752e22046e0937ce6a
Venkata Ravi K A <venkataravikumaralladi@gmail.com>
2021-03-29 12:35:29 +0530

    ids script added

---
+++ NetworkdataClassifier.py
@@ -0,0 +1,112 @@
+# Python imports e.g., os, math...
+import argparse
+import joblib
+import os
+
+# Third party libraries imports
+
+# Numpy imports e.g., numpy,..
+import numpy as np
+
+# Pandas imports e.g., pandas...
+import pandas as pd
+
+# sklearn imports e.g., linear_model,..
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+
+# Azure imports e.g., Workspace,...
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+# VRK: Create TabularDataset using TabularDatasetFactory
+
+# create nsl-kdd network train data
+nsl_kdd_webpath = [
+                          'https://github.com/venkataravikumaralladi/AzureMLCapstoneProject/blob/main/KDDTrain%2B.txt'
+                  ]
+
+#create network analysis data set in tabular format using TabularDatasetFactory
+nsl_kdd_dataset = TabularDatasetFactory.from_delimited_files(path=nsl_kdd_webpath)
+
+
+def clean_data(data):
+    
+    # column names
+    network_data_column_names = [ 
+                  'duration', 'protocol_type', 'service',
+                  'flag', 'src_bytes', 'dst_bytes',
+                  'land', 'wrong_fragment', 'urgent',
+    
+            
+                  'hot', 'num_failed_logins', 'logged_in',
+                  'num_compromised', 'root_shell', 'su_attempted',
+                  'num_root', 'num_file_creations', 'num_shells',
+                  'num_access_files', 'num_outbound_cmds', 'is_hot_login',
+                  'is_guest_login',
+    
+                 
+                  'count', 'srv_count', 'serror_rate',
+                  'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',
+                  'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate',
+                 
+                  'dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate',
+                  'dst_host_diff_srv_rate', 'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate',
+                  'dst_host_serror_rate', 'dst_host_srv_serror_rate', 'dst_host_rerror_rate',
+                  'dst_host_srv_rerror_rate',
+    
+                   'attack_type',
+                   'success_pred' ]
+    
+    
+    train_df = data.to_pandas_dataframe().dropna()
+    train_df.columns = network_data_column_names
+    
+    # For this analysis we drop "success_pred" column
+    train_df.drop('success_pred', axis=1, inplace=True)
+    
+    # Drop attack type in training data which is to be predicted.
+    train_X = train_df.drop("attack_type", axis=1)
+    train_Y = train_df['attack_type']
+    
+    # we build binary classifier for this.
+    train_Y = train_Y.apply(lambda x: 0 if x == 'normal' else 1)    
+    
+    return train_X, train_Y
+    
+
+
+    
+def main():
+    
+    run = Run.get_context()
+    
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--criterion', default='gini', help="The function to measure the quality of a split.")
+    parser.add_argument('--max_depth', type=int, default=None, help="The maximum depth of the tree")
+
+    args = parser.parse_args()
+
+    run.log("Criteria for split:", args.criterion)
+    run.log("Max depth:", args.max_depth)
+    
+    # VRK: Data cleaning step
+    x, y = clean_data(nsl_kdd_dataset )
+    # VRK: Split data into train and test sets.
+    x_train, x_test, y_train, y_test = train_test_split(x,y)
+    
+    decisiontree_attack_classifier = DecisionTreeClassifier(criterion=args.criterion, max_depth=args.max_depth)
+    decisiontree_attack_classifier.fit(x_train, y_train)
+    
+    accuracy = decisiontree_attack_classifier.score(x_test, y_test)
+    
+    #VRK:Save the model.
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(decisiontree_attack_classifier, 'outputs/vrk_bankmodel.joblib')
+    run.log("Accuracy", np.float(accuracy))
+
+if __name__ == '__main__':
+    main()
